3bc22ffa83029f71f0f1f62472f9d3083b11f3c1
Черный Александр <112432741+Aliaksandr3@users.noreply.github.com>
2023-04-26 17:55:02 +0300

    Test

---
+++ app.ts
@@ -0,0 +1,77 @@
+
+
+// lesson 10 task 1 
+// let name = prompt ('Введите имя','');
+// let soname = prompt ('Введите фамилию','');
+// let nikname = prompt ('Введите никнейм',''); 
+// // let Hello = ("name" + "soname" + )
+// if (name != "" $$ name)
+// {alert ((name) + (soname))}
+
+
+
+
+// lesson 10 task 2 
+// let anyNumber = prompt ('Введите любое число', '');
+//     if (anyNumber == 0) 
+//         {
+//             alert ('Просто ноль', '')
+//         }
+//     else 
+//         {
+//             let comparisonResult = (anyNumber < 100) ? 'меньше ста, ':
+//                                    (anyNumber > 100) ? 'больше ста, ':
+//                                                        'ровно сто, ';
+//             let evenOrOdd = (anyNumber % 2 == 0) ? "четное, ":
+//                                                     "не четное, "; 
+//             let negativePositive = (anyNumber < 0) ? "отрицательное, ":
+//                                                      "положительное, ";
+//             let fractionalInteger = (anyNumber-Math.floor (anyNumber) == 0) ? "целое.":
+//                                                                            "дробное.";                                
+//             alert ('Ваше число: ' + comparisonResult + evenOrOdd + negativePositive + fractionalInteger);
+//         }
+
+
+
+// for exemple
+// const a = "a";
+// const b = "b";
+// if (a < b) {
+//   // true
+//   console.log(`${a} is less than ${b}`);
+// } else if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} and ${b} are equal.`);
+// }
+
+// 19.04.2023
+
+// enum animal {
+//     murzic = 'murzic',
+//     maik = 'maik'
+// }
+
+// function bar <T> (value: T): T {
+//     return value
+// }
+// console.log (bar<Namber>(1))
+
+
+// interface ISmt {
+//     a: number;
+//     b: string
+// }
+// interface IObj {
+//     a: number;
+//     b: string;
+//     c: boolean; 
+//     d?: number,
+//     e: ISmt
+// }
+// const obj: IObj = {
+//     a: 1,
+//     b: 'sad'
+//     c: true,
+//     e: 
+// }
